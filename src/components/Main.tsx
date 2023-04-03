d798caebecfce8c5ddaf59ab0bc9696b98a392f8
Hamdi Kamel <hamdi.pirillo4@gmail.com>
2023-04-03 04:36:49 -0700

    added a line scroll feature for desktop

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,6 +2,7 @@ import Section from "./Section"
 import StylesGuide from './StylesGuide'
 import AssetsGuide from "./AssetsGuide"
 import ComponentsGuide from "./ComponentsGuide"
+import { createRef, useEffect } from "react"
 
 const iconSize = '1.2rem'
 
@@ -12,8 +13,22 @@ function Main() {
         ['Components Guide', <ComponentsGuide />]
     ]))
 
+    const lineRef = createRef<HTMLDivElement>()
+
+    useEffect(() => {
+        const { current } = lineRef
+        const line = current as HTMLDivElement
+
+        document.addEventListener('scroll', e => {
+            const y = window.pageYOffset
+            line.style.top = y + 'px'
+        })
+    }, [])
+
     return (
         <main>
+            <div className="line" ref={lineRef}></div>
+
             {list.map(([title, component], i) => (
                 <Section title={title} key={i}>
                     {component}
