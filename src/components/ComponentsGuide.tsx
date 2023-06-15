4dc30e5f877a55929ae0cf0f7c6b698207403704
Hamdi Kamel <hamdi.pirillo4@gmail.com>
2023-06-15 12:56:31 -0700

    added subcomponents

--- src/components/ComponentsGuide.tsx
+++ src/components/ComponentsGuide.tsx
@@ -1,10 +1,33 @@
+const Component: React.FC<{
+    title: string
+    index: number
+}> = ({
+    title,
+    index
+}) => {
+    return (
+        <div className="component">
+            <div className="aside">
+                <span>0{index}</span>
+            </div>
+
+            <div className="inner">
+                <h3>{title}</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores perferendis aut vitae officia ex pariatur porro corrupti officiis inventore explicabo.</p>
+            </div>
+        </div>
+    )
+}
+
+const components = [
+    <Component title='Minimalistic' index={1} />,
+    <Component title='Inspirational' index={2} />
+]
+
 function ComponentsGuide() {
     return (
         <div className="section-guide" id="third">
-            <p style={{
-                fontSize: '2.5rem',
-                textAlign: 'center'
-            }}>No Components</p>
+            {components.map((component) => component)}
         </div>
     )
 }
