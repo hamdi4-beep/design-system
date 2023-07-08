0260010906cba35d1c1a3980e4a9656adb0e9ddf
Hamdi Kamel <hamdi.pirillo4@gmail.com>
2023-07-07 21:11:40 -0700

    created more necessary components for better structure and organization.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,41 @@ const {
     useState
 } = React
 
-function Header() {
+const MenuList: React.FC = () => {
     const [hidden, setHidden] = useState(true)
 
     const handleOpenClick: React.MouseEventHandler<SVGElement> = e => setHidden(!hidden)
 
+    return (
+        <ul className="menu-list">
+            <BsFillGearFill
+                onClick={handleOpenClick}
+                style={{fontSize: '1.4rem'}}
+            />
+
+            <div className={hidden ? 'items-list hidden' : 'items-list'}>
+                <li>
+                    <button onClick={e => location.reload()}>Reset</button>
+                </li>
+
+                <li>
+                    <span>Change Theme</span>
+
+                    <div className="colors-list">
+                        <div className="color one"></div>
+                        <div className="color two"></div>
+                        <div className="color three"></div>
+                        <div className="color four"></div>
+                    </div>
+                </li>
+
+                <li>Hide Interface</li>
+            </div>
+        </ul>
+    )
+}
+
+function Header() {
     return (
         <header>
             <h1>Design System</h1>
@@ -30,31 +60,7 @@ function Header() {
                     </div>
                 </div>
 
-                <ul className="menu-list">
-                    <BsFillGearFill
-                        onClick={handleOpenClick}
-                        style={{fontSize: '1.4rem'}}
-                    />
-
-                    <div className={hidden ? 'items-list hidden' : 'items-list'}>
-                        <li>
-                            <button onClick={e => location.reload()}>Reset</button>
-                        </li>
-
-                        <li>
-                            <span>Change Theme</span>
-
-                            <div className="colors-list">
-                                <div className="color one"></div>
-                                <div className="color two"></div>
-                                <div className="color three"></div>
-                                <div className="color four"></div>
-                            </div>
-                        </li>
-
-                        <li>Hide Interface</li>
-                    </div>
-                </ul>
+                <MenuList />
             </div>
         </header>
     )
